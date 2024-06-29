a77363a0ecece3bd1b34f1f7256bed79fa015090
najmiddino <164999098+najmiddino@users.noreply.github.com>
2024-06-29 14:50:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let ism = prompt('ismingizni kiriting')
+let login = prompt('logini kiriting')
+let passworld = prompt('pasworldingizni kiriting')
+
+let obj = {
+}
+obj.name = ism
+obj.special = login
+obj.spacePassword = passworld
+
+console.log(obj);
